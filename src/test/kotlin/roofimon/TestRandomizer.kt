a602061a4e83594543f80f2a332a0a62c120eb06
roofimon <roofimon@gmail.com>
2016-04-12 21:38:47 +0700

    Extract initial function remove duplicate

--- src/test/kotlin/roofimon/TestRandomizer.kt
+++ src/test/kotlin/roofimon/TestRandomizer.kt
@@ -26,23 +26,27 @@ class Randomizer(val random: Random) {
 }
 class TestRandomizer {
     val stubRandom: Random = mock()
+    var randomizer:Randomizer? = null
+
+    fun initial(expectedValue:Int) {
+        doReturn(expectedValue).`when`(stubRandom).nextInt()
+        randomizer = Randomizer(stubRandom)
+    }
+
     @Test fun testRandomPattern() {
-        doReturn(2).`when`(stubRandom).nextInt()
-        val randomizer = Randomizer(stubRandom)
-        assertEquals(2, randomizer.randomPattern())
+        this.initial(2)
+        assertEquals(2, randomizer?.randomPattern())
         verify(stubRandom, atMost(1)).setSeed(2)
     }
     @Test fun testRandomOperand() {
-        doReturn(9).`when`(stubRandom).nextInt()
-        val randomizer = Randomizer(stubRandom)
-        assertEquals(9, randomizer.randomOperand())
+        this.initial(9)
+        assertEquals(9, randomizer?.randomOperand())
         verify(stubRandom, atMost(1)).setSeed(9)
     }
 
     @Test fun testRandomOperator() {
-        doReturn(1).`when`(stubRandom).nextInt()
-        val randomizer = Randomizer(stubRandom)
-        assertEquals(1, randomizer.randomOperator())
+        this.initial(1)
+        assertEquals(1, randomizer?.randomOperator())
         verify(stubRandom, atMost(1)).setSeed(3)
     }
 }
